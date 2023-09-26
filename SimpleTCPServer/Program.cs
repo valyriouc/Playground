31fb4965a76869257e79254b93fcd7fc439a20ea
Valacor <valyrious.informatics@gmail.com>
2023-09-26 22:23:45 +0200

    Implement simple tcp server

---
+++ SimpleTCPServer/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Net;
+using System.Net.Sockets;
+
+namespace SimpleTCPClient;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        IPAddress address = IPAddress.Parse("127.0.0.1");
+
+        TcpListener listener = new TcpListener(address, 4454);
+
+        listener.Start();
+        Console.WriteLine("Listening...");
+
+        while (true)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Connection received");
+
+            using Stream stream = client.GetStream();
+
+            using StreamReader reader = new StreamReader(stream);
+
+            Console.WriteLine("<<<<<< Content >>>>>");
+            Console.WriteLine(reader.ReadToEnd());
+        }
+    }
+}
