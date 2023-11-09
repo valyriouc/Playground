17b7655f55e25659b51eea59024c1b467cabf58a
Valacor <valyrious.informatics@gmail.com>
2023-11-09 19:11:00 +0100

    Starting implementation of dynamic stack

---
+++ ASimpleStack/DynamicSizeStack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASimpleStack
+{
+    public class DynamicSizeStack<TEntity> : Stack<TEntity>
+    {
+        private int Offset { get; set; }
+
+        private List<TEntity> Buffer { get; set; }
+
+        public int Size => Buffer.Count;
+
+        public int Capacity => Buffer.Capacity;
+
+        public override bool IsEmpty => Offset == 0;
+
+        public DynamicSizeStack()
+        {
+            Offset = 0;
+            Buffer = new List<TEntity>();
+        }
+
+        public override void Push(TEntity entity)
+        {
+            if (Buffer is null)
+            {
+                throw new Exception(
+                    "Stack not initialized!");
+            }
+
+            Offset += 1;
+            Buffer.Add(entity);
+        }
+
+        public override TEntity Pop() =>
+            GetInternal();
+
+        public override TEntity Top() => 
+            GetInternal();
+
+        private TEntity GetInternal()
+        {
+            if (Offset == 0)
+            {
+                throw new IndexOutOfRangeException(
+                    "Stack is empty");
+            }
+
+            Offset -= 1;
+            TEntity entity = Buffer[Offset];
+            Buffer.Remove(entity);
+            Buffer.Capacity -= 1;
+            return entity;
+        }
+    }
+}
---
+++ StackTests/DynamicSizeStackTests.cs
@@ -0,0 +1,57 @@
+﻿using ASimpleStack;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace StackTests
+{
+    public class DynamicSizeStackTests
+    {
+        [Fact]
+        public void PushAddsEntityToStack()
+        {
+            DynamicSizeStack<int> stack = new DynamicSizeStack<int>();
+
+            stack.Push(1);
+
+            Assert.Equal(1, stack.Pop());
+        }
+
+        [Fact]
+        public void PopThrowsExceptionWhenStackSizeIsZero() 
+        {
+            DynamicSizeStack<int> stack = new DynamicSizeStack<int>();
+
+            Assert.Throws<IndexOutOfRangeException>(() => stack.Pop());
+        }
+
+        [Fact]
+        public void PopReturnsCorrectEntryAndDecrementListSize()
+        {
+            DynamicSizeStack<int> stack = new DynamicSizeStack<int>();
+
+            stack.Push(1);
+
+            Assert.Equal(1, stack.Size);
+            Assert.Equal(1, stack.Capacity);
+
+            int item = stack.Pop();
+            Assert.Equal(1, item);
+            Assert.Equal(0, stack.Size);
+            Assert.Equal(0, stack.Capacity);
+        }
+
+        [Fact]
+        public void TopThrowsExceptionWhenStackSizeIsZero()
+        {
+            DynamicSizeStack<int> stack = new DynamicSizeStack<int>();
+
+            Assert.Throws<IndexOutOfRangeException>(() => stack.Top());
+        }
+    }
+}
