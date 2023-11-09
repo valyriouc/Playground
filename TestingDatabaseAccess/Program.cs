ee552383e28158fb587f88c3ca70946f64f85845
Valacor <valyrious.informatics@gmail.com>
2023-11-09 19:11:24 +0100

    Creating database access prototype

---
+++ TestingDatabaseAccess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace TestingDatabaseAccess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression<Func<int>> expr = () => 2 + 2;
+        }
+    }
+}
