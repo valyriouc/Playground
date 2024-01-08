7ffdf99f6046b55c365d5933058bb86e8b9a0319
Valacor <valyrious.informatics@gmail.com>
2024-01-08 10:47:19 +0100

    Implement liskov prototype

---
+++ LiskovTesting/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Data;
+
+namespace LiskovTesting;
+
+internal interface IGeometryFigure
+{
+    public double CalcSurface();
+}
+
+internal class Quadrat : IGeometryFigure
+{
+    public int Breite { get; set; }
+
+    public double CalcSurface()
+    {
+        return Breite * Breite;
+    }
+}
+
+internal class Rechteck : IGeometryFigure
+{
+    public int Breite { get; set; }
+
+    public int Hoehe { get; set; }
+
+    public double CalcSurface()
+    {
+        return Breite * Hoehe;
+    }
+}
+
+internal static class Program
+{
+    public static void Main()
+    {
+        Quadrat quad = new Quadrat();
+        quad.Breite = 5;
+
+        double res1 = Calculate(quad);
+        Console.WriteLine(res1);
+
+        Rechteck recht = new Rechteck();
+        recht.Hoehe = 2;
+        recht.Breite = 4;
+
+        double res2 = Calculate(recht);
+        Console.WriteLine(res2);
+    }
+
+    public static double Calculate(IGeometryFigure figure)
+    {
+        return figure.CalcSurface();
+    }
+}
