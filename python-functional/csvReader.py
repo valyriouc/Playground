b07020c5981cd85c40a8cb60123ef428747ac8bf
Valacor <valyrious.informatics@gmail.com>
2024-01-15 10:20:18 +0100

    Finish basic csv reader

--- python-functional/csvReader.py
+++ python-functional/csvReader.py
@@ -1,16 +1,79 @@
 from collections.abc import Iterator
 from typing import Iterator
-from io import TextIOWrapper
+import requests
+
+class CsvError(Exception):
+    def __self__(self, message):
+        super().__init__(message)
+
+class CsvRow:
+    def __init__(self, args: list[any]):
+        self.cells = args
+        self.count = len(args)
+
+        self._internal_count = 0
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self._internal_count == self.count:
+            self._internal_count = 0
+            raise StopIteration
+        else:
+            current = self.cells[self._internal_count]
+            self._internal_count += 1
+            return current
+        
+    def __getitem__(self, x):
+        return self.cells[x]
 
 class CsvTable:
+    allowed = [";", ","]
     def __init__(self):    
-        self.headColumnNames = []
         self.rowCount = 0
         self.columnCount = 0
-        self.content = []
+        self.content: list[CsvRow] = []
+        self.separation = None
 
-    def add_line(self, line: str, isHead: bool = False):
-        pass
+        self._iter_count = 0
+
+    def add_line(self, line: str) -> None:
+        if self.separation is None:
+            self.separation = self._get_separation_sign(line)
+        if (self.columnCount == 0):
+            columnCount = len(line.split(self.separation))
+            if (columnCount == 0):
+                raise CsvError("Csv has no content!")
+        # TODO: analyse the line to make sure it just contains separator of <separation>
+        if not self._is_correct_separactor(line):
+            raise CsvError(f"Detected incorrect csv separator at line {self.rowCount + 1}")
+        content = [i.strip() for i in line.split(self.separation)]
+        self.content.append(CsvRow(content))
+        self.rowCount += 1
+
+    def _get_separation_sign(self, line) -> str: 
+        analyseable = [i for i in line if i in CsvTable.allowed]
+        return max(analyseable)
+    
+    def _is_correct_separactor(self, line) -> bool:
+        return True
+    
+    def __iter__(self):
+        return self
+
+    def __next__(self): 
+        if (self._iter_count == self.rowCount):
+            self._iter_count = 0
+            raise StopIteration
+        else:
+            row = self.content[self._iter_count]
+            self._iter_count += 1
+            return row
+        
+    # Accessing operator 
+    def __getitem__(self, key):
+        return self.content[key[0]][key[1]]
 
 class SplitAtLineEndIter:
     def __init__(self, input: str):
@@ -59,21 +122,23 @@ class CsvReader:
     def read_from_string(csv: str) -> CsvTable:
         table = CsvTable()
         for line in SplitAtLineEndIter(csv):
-            # table.add_line(line)
-            print(line, end=" ")
+            table.add_line(line)
         return table
             
     @staticmethod 
-    def read_from_inet(url: str) -> CsvTable:
-        pass
-
+    def read_from_inet(url: str) -> CsvTable | None:
+        response = requests.get(url) 
+        if response.status_code == 200:
+            return CsvReader.read_from_string(response.content)            
+        return None
+    
 if __name__ == "__main__":
-    CsvReader.read_from_file("classdefinition.csv")
-
-    print()
-
+    table = CsvReader.read_from_file("classdefinition.csv")
     CsvReader.read_from_string("c1,c2,c3\r1,2,3\r1,2,3")
-
-    print()
-
     CsvReader.read_from_string("c1,c2,c3\n1,2,3\n1,2,3\n")
+    CsvReader.read_from_string("c1;c2;c3\n1;2;3\n1;2;3\n")
+
+
+    for row in table:
+        print(row[1], end=" ")
+        print(row[0])
