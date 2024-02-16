d5ba79940442cd3703b7f00988b1d9eb77ece0c8
Valacor <valyrious.informatics@gmail.com>
2024-02-16 20:02:27 +0100

    First event wait handle prototype

---
+++ EventWaitHandlesTesting/Program.cs
@@ -0,0 +1,13 @@
+﻿
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        EventWaitHandle myHandle = new EventWaitHandle(false, EventResetMode.AutoReset, "myhandle");
+
+        Console.WriteLine("We are now waiting for a signal...!");
+        myHandle.WaitOne();
+        Console.WriteLine("We are finished with waiting!");
+        Console.WriteLine("Shutting down!");
+    }
+}
---
+++ WaitHandleSetter/Program.cs
@@ -0,0 +1,15 @@
+﻿
+internal static class Program
+{
+    public static void Main()
+    {
+        EventWaitHandle handle = new EventWaitHandle(false, EventResetMode.AutoReset, "myhandle");
+
+        Console.WriteLine("We are waiting 5 seconds!");
+        Thread.Sleep(5000);
+        Console.WriteLine("Now everybody gets notified!");
+        handle.Set();
+
+        Console.WriteLine("Everything done!");
+    }
+}
